ed0cb8526d6275cffb199134b26699fbcec7091b
vt9118 <vt939260@outlook.com>
2025-06-29 11:44:56 +0530

    This program is use for make a list in python
    With the help of this program . We can create a list .

---
+++ pro_for_make a list.py
@@ -0,0 +1,44 @@
+# men = {"kela": "Banana", "Aaam":"Mango", "Anar": "Pomegranate", "Kharbooja": "Melon"}
+# fruits = input("Enter the name of your favourite fruit: ")
+# print(men[fruits])
+
+#1 practice project completed
+
+# n = set()
+# i = input("Enter the number :")
+# n.add(int(i))
+
+# i = input("Enter the number :")
+# n.add(int(i))
+
+# i = input("Enter the number :")
+# n.add(int(i))
+
+# i = input("Enter the number :")
+# n.add(int(i))
+
+# i = input("Enter the number :")
+# n.add(int(i))
+
+# i = input("Enter the number :")
+# n.add(int(i))
+
+# i = input("Enter the number :")
+# n.add(int(i))
+
+# i = input("Enter the number :")
+# n.add(int(i))
+
+# i = input("Enter the number :")
+# n.add(int(i))
+# print(n)
+
+# 2nd practice project completed
+
+dic = {}
+question = int(input("How many key-value pairs do you want to enter?: "))
+for i in range (question):
+    key = input("Enter your name: ")
+    value = input("Enter your favourite language: ")
+    dic[key] = value
+print(dic)
